926e829ead8c83c07548445ba78bbe05001fae20
kotpilun <kotpilun3m@gmail.com>
2023-02-08 19:37:31 +0300

    feat: make task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,26 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        // this.middle = Math.floor((this.min + this.max) / 2);
+        this.middle = Math.ceil((this.max - this.min) / 2 + this.min);
+        return this.middle;
     }
 
     lower() {
-
+        // this.max = this.middle - 1;
+        this.max = this.middle;
+        console.log('Верхняя граница', this.max);
     }
 
     greater() {
-
+        // this.min = this.middle + 1;
+        this.min = this.middle;
+        console.log('Нижняя граница', this.min);
     }
 }
 
